7d06fa05bd91facf14ae59ca84e9d4788f6c5b91
DanielNaranjo <dnartor.98@gmail.com>
2022-01-26 19:19:45 +0000

    Primer build

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import "./index.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "materialize-css/dist/css/materialize.min.css";
