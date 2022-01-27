f2dc3bb20f24548c276946a84c270ddb7f75875a
DanielNaranjo <dnartor.98@gmail.com>
2022-01-27 19:21:45 +0000

    restore fonts

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,7 @@ import React from "react";
 import "./index.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "materialize-css/dist/css/materialize.min.css";
-import 'animate.css';
-
+import "animate.css";
 
 import Header from "./components/Header.jsx";
 import Home from "./components/Home.jsx";
@@ -17,18 +16,18 @@ function App() {
   return (
     <Router>
       <header>
-      <Header/>
+        <Header />
       </header>
       <main>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/clasificacion" element={<Clasificacion />} />
-        <Route path="/calendario" element={<Calendario />} />
-        <Route path="/circuitos" element={<Circuitos />} />
-        <Route path="/encliclopedia" element={<Circuitos />} />
-        <Route path="/enciclopedia" element={<Enciclopedia />} />
-        <Route path="/noticias" element={<Noticias />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/clasificacion" element={<Clasificacion />} />
+          <Route path="/calendario" element={<Calendario />} />
+          <Route path="/circuitos" element={<Circuitos />} />
+          <Route path="/encliclopedia" element={<Circuitos />} />
+          <Route path="/enciclopedia" element={<Enciclopedia />} />
+          <Route path="/noticias" element={<Noticias />} />
+        </Routes>
       </main>
       <footer></footer>
     </Router>
