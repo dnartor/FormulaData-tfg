9f60e589cb3bf64c94064cb1325e79e6d002681c
DanielNaranjo <dnartor.98@gmail.com>
2022-02-15 20:59:09 +0000

    Vista de noticia individualpc terminada

--- src/components/noticias/AllNoticias.jsx
+++ src/components/noticias/AllNoticias.jsx
@@ -1,29 +1,14 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 import Buscador from "../Buscador";
 const Noticias = () => {
   
 
-  const [apiCall, guardarApiCall] = useState(true);
-  const apiKey = "pub_4219973489f9fd3dcf5d2e8be0f5b30fe9d9";
-  const [noticias, guardarNoticias] = useState([]);
+ 
   const [busqueda, guardarBusqueda] = useState("");
 
 
-  useEffect(() => {
-    const clienteApi = async () => {
-      if (apiCall === false) return;
-
-      const url = `https://newsdata.io/api/1/news?apikey=${apiKey}&q=Formula%201&language=en&category=sports`;
-      let resultado = await axios.get(url);
-      resultado = resultado.data.results;
-      guardarNoticias(resultado.slice(0, 3));
-      //guardarNoticias(resultado);
-    };
-    //clienteApi();
-  });
-
+  
   
   return (
     <div className="mi_container">
